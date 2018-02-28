73e5413fb88b2964a53cd5e90f86c006282bfe85
zhanglixiang <zhanglixiang@yuantiaotech.com>
2018-02-28 11:25:24 +0800

    ele

--- vueTest/src/router/index.js
+++ vueTest/src/router/index.js
@@ -1,18 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/Login'
-import Parent from '@/components/Parent'
+import Login from '@/components/page/Login'
+import Child from '@/components/page/Child'
+import Home from '@/components/common/Home'
+import Choice from '@/components/page/Choice'
+import Song from '@/components/page/Song'
+import Singer from '@/components/page/Singer'
+import Rank from '@/components/page/Rank'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      component: Login
+      redirect: '/login'
     },
     {
-      path: '/parent',
-      component: Parent
+    	path: '/home',
+    	component: Home,
+    	children: [
+    		{
+    			path: '/choice',
+    			component: Choice
+    		},
+    		{
+    			path: '/song',
+    			component: Song
+    		},
+    		{
+    			path: '/singer',
+    			component: Singer
+    		},
+    		{
+    			path: '/rank',
+    			component: Rank
+    		}
+    	]
+    },
+    {
+    	path: '/login',
+    	component: Login
     }
   ]
 })
